c7a7b3484923c4e0e1164e75124424c83947a24d
mridul <mridulvai46@gmail.com>
2025-10-11 13:14:04 +0530

    check vercel error

--- Job-Portal/server/utils/multer.js
+++ Job-Portal/server/utils/multer.js
@@ -1,21 +1,6 @@
-// utils/multer.js
 import multer from "multer";
-import path from "path";
-import fs from "fs";
 
-const uploadDir = "uploads";
-
-// create folder if not exist
-if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
+
 export default upload;
